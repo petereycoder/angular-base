46786c590aa260b69e8eb3223791938861b8459f
petereycoder <fp.pipe.reyes@gmail.com>
2024-07-22 14:15:36 -0600

    Pruebas de angular

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,27 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+  <hr>
+  <h3>Counter: {{counter}}</h3>
+  <button (click)="increaseBy(1)">+1</button>
+  <button (click)="functionReset()">Reset</button>
+  <button (click)="increaseBy(-1)">-1</button>
+
+  `
+})
+
+export class CounterComponent {
+  constructor() { }
+  public counter: number = 10;
+  increaseBy(value:number):void{
+
+      this.counter += value;
+
+  }
+
+  functionReset():void{
+    this.counter = 10;
+  }
+}
